08dc627663651fd11ae074991145a48f6df4bb0f
artaasadi <2000.arta@gmail.com>
2022-12-18 11:57:16 +0330

    adding rows added

--- database.py
+++ database.py
@@ -1,2 +1,20 @@
 import os
 import psycopg2
+
+class Db:
+    '''initiate the database configuration'''
+    def __init__(self):
+        self.conn = psycopg2.connect(
+            host="esme.iran.liara.ir",
+            port=33846,
+            database="relaxed_proskuriakova",
+            user='root',
+            password='pB7kP1kf0jDiLHwGtGDtBJrA')
+
+    def add_ad(self, email, description, url, category, state):
+        cur = self.conn.cursor()
+        cur.execute('INSERT INTO ads (email, description, url, cateory, state) VALUES (%s, %s, %s, %s, %s)',
+                    (email, description, url, category, state))
+        self.conn.commit()
+
+    
