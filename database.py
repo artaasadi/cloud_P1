85c22e859d5ff1ad57c5440ec21c5463683a9481
artaasadi <2000.arta@gmail.com>
2022-12-18 12:09:29 +0330

    update commands added

--- database.py
+++ database.py
@@ -13,8 +13,21 @@ class Db:
 
     def add_ad(self, email, description, url, category, state):
         cur = self.conn.cursor()
-        cur.execute('INSERT INTO ads (email, description, url, cateory, state) VALUES (%s, %s, %s, %s, %s)',
+        cur.execute('INSERT INTO ads (email, description, url, cateory, state) VALUES (%s, %s, %s, %s, %s);',
                     (email, description, url, category, state))
         self.conn.commit()
 
-    
+    def update_category(self, category, id):
+        cur = self.conn.cursor()
+        cur.execute('UPDATE ads SET category = %s WHERE id = %s;', (category, id))
+        self.conn.commit()
+
+    def update_state(self, state, id):
+        cur = self.conn.cursor()
+        cur.execute('UPDATE ads SET state = %s WHERE id = %s;', (state, id))
+        self.conn.commit()
+
+    def get_by_id(self, id):
+        cur = self.conn.cursor()
+        cur.execute('SELEFT * FROM ads WHERE id = %s;', (id))
+        return cur.fetchall()
